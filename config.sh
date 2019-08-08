cb1873a1700cf4da741531cfb7a9ec1c959cec4c
Griffin <bluedogerino@gmail.com>
2019-08-08 21:27:27 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -9,19 +9,19 @@ KBUILD_BUILD_HOST="GriffinsCloud"
 export oem=samsung
 export device=j5y17lte
 
-if [[ ROMname="LineageOS" ]]; then
+if [[ -z ROMname="LineageOS" ]]; then
 ROM="LineageOS"
 manifest_url="https://github.com/LineageOS/android"
 export rom_vendor_name="lineage"
 branch="lineage-16.0"
 echo "using LineageOS"
-elif [[ ROMname="RR" ]]; then
+elif [[ -z ROMname="RR" ]]; then
 ROM="RR"
 manifest_url="https://github.com/ResurrectionRemix/platform_manifest"
 export rom_vendor_name="rr"
 branch="pie"
 echo "using RR"
-elif [[ ROMname="AOSiP" ]]; then
+elif [[ -z ROMname="AOSiP" ]]; then
 ROM="RR"
 ROM="AOSiP"
 manifest_url="https://github.com/AOSiP/platform_manifest"
