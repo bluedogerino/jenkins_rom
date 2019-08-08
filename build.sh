a7212bc57cdbd095d14a6c5d2c147a1f4bb68329
Griffin <bluedogerino@gmail.com>
2019-08-08 22:03:52 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ KBUILD_BUILD_HOST="GriffinsCloud"
 export oem=samsung
 export device=j5y17lte
 
-if [[ $ROMname == "LineageOS" ]]; then
+if [[ $ROMname = "LineageOS" ]]; then
 ROM="LineageOS"
 manifest_url="https://github.com/LineageOS/android"
 export rom_vendor_name="lineage"
@@ -22,7 +22,7 @@ manifest_url="https://github.com/ResurrectionRemix/platform_manifest"
 export rom_vendor_name="rr"
 branch="pie"
 echo "using RR"
-elif [[ $ROMname == "AOSiP" ]]; then
+elif [[ $ROMname = "AOSiP" ]]; then
 ROM="RR"
 manifest_url="https://github.com/AOSiP/platform_manifest"
 export rom_vendor_name="aosip"
