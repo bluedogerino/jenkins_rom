fb55f48c7e82373f32c02a97151977e9b466c640
Griffin <bluedogerino@gmail.com>
2019-08-08 18:25:02 +0200

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GITHUB_USER=Griffin
+GITHUB_EMAIL=bluedogerino@gmail.com
+
+KBUILD_BUILD_USER="griffin"
+KBUILD_BUILD_HOST="BlueDrone"
+
+export oem=samsung
+export device=j5y17lte
+
+ROM="LineageOS"
+manifest_url="https://github.com/LineageOS/android"
+export rom_vendor_name="lineage" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+branch="lineage-16.0"
+
+release_repo="bluedogerino/symmetrical-broccoli"
+
+timezone="Europe/Sarajevo"
