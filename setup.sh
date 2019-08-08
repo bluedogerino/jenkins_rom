f5eae45f4aa13f3b91ec1d9c59791534305b899d
Griffin <bluedogerino@gmail.com>
2019-08-08 19:52:33 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,4 @@ echo $TELEGRAM_TOKEN >/tmp/tg_token
 echo $TELEGRAM_CHAT >/tmp/tg_chat
 echo $GITHUB_TOKEN >/tmp/gh_token
 
-sudo cp github-release /usr/bin
-sudo cp telegram /usr/bin
 sudo ./build.sh
